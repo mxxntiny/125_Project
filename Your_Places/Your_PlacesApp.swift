5fde86d161d5c4e5e710b542505bf667fddf0461
Aidan Huerta <aidanmh@uci.edu>
2026-01-29 10:53:20 -0800

    Initial Commit

---
+++ Your_Places/Your_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_PlacesApp.swift
+//  Your_Places
+//
+//  Created by Aidan Huerta on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Your_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
